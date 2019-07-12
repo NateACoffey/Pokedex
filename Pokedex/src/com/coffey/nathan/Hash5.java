1b7751b157c6332639917be368cf7290a9cc6177
Nathan Coffey <nathan.coffey@snhu.edu>
2019-07-12 01:21:05 -0700

    Separated hash method to its own class so any class can use it

---
+++ Pokedex/src/com/coffey/nathan/Hash5.java
@@ -0,0 +1,20 @@
+package com.coffey.nathan;
+
+public class Hash5 {
+	
+	public int hashName(String name) { //hashes names of pokemon for array insertion
+		int numb = 0;
+        
+        for(int i = 0; i < name.length(); i++){
+            if(i > 5) //only takes up to first 5 characters
+               break; 
+            
+            int temp = name.charAt(i);
+            
+            numb += temp * (int)Math.pow(7, i);
+        }
+        
+        return numb % 807;
+		
+	}
+}
