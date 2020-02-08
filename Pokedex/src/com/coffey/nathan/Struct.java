7f7f5ed601be3ed31460c189cfbacf97cce7dfe1
Nathan Coffey <nathan.coffey@snhu.edu>
2020-02-07 20:16:01 -0800

    Set default values/changed class access modifier

--- Pokedex/src/com/coffey/nathan/Struct.java
+++ Pokedex/src/com/coffey/nathan/Struct.java
@@ -1,23 +1,23 @@
 package com.coffey.nathan;
 
-public class Struct {
+class Struct {
 	
-	String name;
+	String name = "N/A";
 	int number = 0;
 	
-	String picture;
+	String picture = "";
 	
-	String type1;
-	String type2;
+	String type1 = "(none)";
+	String type2 = "(none)";
 	
-	String ability1;
-	String ability2;
-	String hiddenAbility;
+	String ability1 = "(none)";
+	String ability2 = "(none)";
+	String hiddenAbility = "(none)";
 	
 	int catchRate = 0;
 	
-	String eggGroup1;
-	String eggGroup2;
+	String eggGroup1 = "(none)";
+	String eggGroup2 = "(none)";
 	
 	int baseHP = 0;
 	int baseAttack = 0;
