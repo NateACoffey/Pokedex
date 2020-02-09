7da507cae17229603dbb5f454abd4a3bc4a5d31e
Nathan Coffey <nathan.coffey@snhu.edu>
2020-02-08 21:41:12 -0800

    Changed hash to hash all characters

--- Pokedex/src/com/Coffey/Nathan/Struct/Hash5.java
+++ Pokedex/src/com/Coffey/Nathan/Struct/Hash5.java
@@ -5,16 +5,14 @@ public class Hash5 {
 	public int hashName(String name) { //hashes names of pokemon for array insertion
 		int numb = 0;
         
-        for(int i = 0; i < name.length(); i++){
-            if(i > 5) //only takes up to first 5 characters
-               break; 
+        for(int i = 0; i < name.length(); i++){ 
             
             int temp = name.charAt(i);
             
             numb += temp * (int)Math.pow(7, i);
         }
         
-        return numb % 890;
+        return Math.abs(numb % 890);
 		
 	}
 }
