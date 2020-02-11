c404acbdf6d9a15dc63b1e30c262ed0703c55c29
Nathan Coffey <nathan.coffey@snhu.edu>
2020-02-10 18:23:29 -0800

    Fixed bug with calling bucket with no assigned values

--- Pokedex/src/com/Coffey/Nathan/Struct/FillStruct.java
+++ Pokedex/src/com/Coffey/Nathan/Struct/FillStruct.java
@@ -39,7 +39,7 @@ public class FillStruct {
 		Hash5 h5 = new Hash5();
 		int bucket = h5.hashName(pokeName);
 		
-		curr = hash[bucket].head;
+		curr = (hash[bucket] != null) ? hash[bucket].head : null ;	//verifies the bucket is not null
 		
 		while(curr != null) {
 			if(curr.getName().toLowerCase().contentEquals(pokeName)) {
